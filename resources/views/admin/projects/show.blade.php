61333031792ca34982d2f13f2c52d307891bca31
rebs-code <jack.rebussi@gmail.com>
2024-02-12 16:56:57 +0100

    feat: show tech label in show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -10,10 +10,11 @@
                 @if ($project->type)
                     <p>{{ $project->type->name }}</p>
                 @endif
-                {{-- @if ($project->image)
-                    <img src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->name }}" class="img-fluid"
-                        style="max-width: 300px;">
-                @endif --}}
+                <div>
+                    @foreach ($project->technologies as $tech)
+                        <span class="badge bg-secondary">{{ $tech->name }}</span>
+                    @endforeach
+                </div>
                 <p>{{ $project->repo_link }}</p>
                 <p>{{ $project->slug }}</p>
                 <a href="{{ route('admin.projects.index') }}" role="button" class="btn btn-primary">Back</a>
