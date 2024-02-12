ea6a9ca5393597579ce72fc55aaefa0ae857e7e4
rebs-code <jack.rebussi@gmail.com>
2024-02-12 15:48:59 +0100

    feat: populate seeder with technologies array

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,17 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['HTML', 'CSS', 'Javascript', 'Vue', 'Laravel', 'Bootstrap', 'Tailwind', 'React', 'Angular', 'Node', 'MySQL', 'Vite'];
+
+        foreach ($technologies as $technology) {
+
+            $new_technology = new Technology();
+
+            $new_technology->name = $technology;
+            //use the Str class to slugify the name
+            $new_technology->slug = Str::of($technology)->slug('-');
+
+            $new_technology->save();
+        }
     }
 }
